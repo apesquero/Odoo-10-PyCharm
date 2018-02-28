50737531ed5655c5e79ceea6ac5693252985eb3d
apesquero <apesquero@gmail.com>
2018-02-28 23:36:13 +0100

    Update changes

---
+++ custom/addons-essentials/todo_task/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'To-Do Application',
+    'description': 'Manage your personal To-Do task.',
+    'author': 'Daniel Reis',
+    'depends': ['base'],
+    'data': ['security/ir.model.access.csv',
+             'security/todo_access_rules.xml',
+             'views/todo_menu.xml',
+             'views/todo_task.xml'],
+    'application': True,
+}
