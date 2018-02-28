5229194b644f0209c8241ae1dea1128458edf6d9
apesquero <apesquero@gmail.com>
2018-02-28 23:09:24 +0100

    Update changes

--- custom/addons-essentials/todo_user/__manifest__.py
+++ custom/addons-essentials/todo_user/__manifest__.py
@@ -4,6 +4,6 @@
     'name': 'Multiuser To-Do',
     'description': 'Extend the To-Do app to multiuser.',
     'author': 'Daniel Reis',
-    'depends': ['todo_app'],
-    'data': ['views/todo_task.xml'],
+    'depends': ['todo_task', 'mail'],
+    'data': ['views/todo_user.xml'],
 }
--- custom/addons-essentials/todo_user/models/__init__.py
+++ custom/addons-essentials/todo_user/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import todo_task
+from . import todo_user
