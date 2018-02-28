ede0d0a6b57c868e2ed055c235df0690aa83d9db
apesquero <apesquero@gmail.com>
2018-02-28 23:52:40 +0100

    Update changes

--- custom/addons-essentials/todo_user/__manifest__.py
+++ custom/addons-essentials/todo_user/__manifest__.py
@@ -5,5 +5,6 @@
     'description': 'Extend the To-Do app to multiuser.',
     'author': 'Daniel Reis',
     'depends': ['todo_task', 'mail'],
-    'data': ['views/todo_user.xml'],
+    'data': ['views/todo_user.xml',
+             'security/todo_access_rules.xml'],
 }
