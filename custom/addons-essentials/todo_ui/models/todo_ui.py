dd41a81feb220088c3d7649414f314dd48ea3080
apesquero <apesquero@gmail.com>
2018-03-02 20:58:25 +0100

    Update changes

--- custom/addons-essentials/todo_ui/models/todo_ui.py
+++ custom/addons-essentials/todo_ui/models/todo_ui.py
@@ -13,7 +13,26 @@ class Stage(models.Model):
     _description = 'To-do Stage'
     _order = 'sequence,name'
 
+    # String fields
     name = fields.Char('Name', 40, translate=True)
+    desc = fields.Text('Description')
+    state = fields.Selection([('draft','Draft'),
+                              ('open','Open'),
+                              ('done','Done')],
+                             'State')
+    docs = fields.Html('Documentation')
+
+    # Numeric fields
     sequence = fields.Integer('Sequence')
+    perc_complete = fields.Float('% Complete', (3,2))
+
+    # Date fields
+    date_effective = fields.Date('Effective Date')
+    date_changed = fields.Datetime('Last Changed')
+
+    # Other fileds
+    fold = fields.Boolean('Folded?')
+    image = fields.Binary('Image')
+
 
 
