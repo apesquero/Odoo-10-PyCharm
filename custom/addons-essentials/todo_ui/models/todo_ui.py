9a934862c8ba10a4145f39bdf359523cf31d3119
apesquero <apesquero@gmail.com>
2018-03-03 00:31:55 +0100

    Update changes

--- custom/addons-essentials/todo_ui/models/todo_ui.py
+++ custom/addons-essentials/todo_ui/models/todo_ui.py
@@ -6,7 +6,10 @@ class Tag(models.Model):
     _name = 'todo.task.tag'
     _description = 'To-do Tag'
 
-    name = fields.Char('Name', 40, translate=True)
+    name = fields.Char('Name', size=40, translate=True)
+
+    # Relation fields
+    # tag_ids = fields.Many2many('todo.task')
 
 class Stage(models.Model):
     _name = 'todo.task.stage'
@@ -14,7 +17,7 @@ class Stage(models.Model):
     _order = 'sequence,name'
 
     # String fields
-    name = fields.Char('Name', 40, translate=True)
+    name = fields.Char('Name', size=40, translate=True)
     desc = fields.Text('Description')
     state = fields.Selection([('draft','Draft'),
                               ('open','Open'),
@@ -30,9 +33,20 @@ class Stage(models.Model):
     date_effective = fields.Date('Effective Date')
     date_changed = fields.Datetime('Last Changed')
 
-    # Other fileds
+    # Other fields
     fold = fields.Boolean('Folded?')
     image = fields.Binary('Image')
 
+    # Relation fields
+    # stage_id = fields.One2many()
 
+class TodoTask(models.Model):
+    _inherit = 'todo.task'
 
+    stage_id = fields.Many2one('todo.task.stage', 'Stage')
+
+    tag_ids = fields.Many2many('todo.task.tag',     # related model
+                               'todo_task_tag_rel', # relation table name
+                               'task_id',           # field for "this" record
+                               'tag_id',            # field for "other" record
+                               strig='Tags')
