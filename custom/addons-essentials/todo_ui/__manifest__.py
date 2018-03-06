33d256a6adf5d95e4a0ef457f399ab37de407093
apesquero <apesquero@gmail.com>
2018-03-07 00:18:03 +0100

    Update changes

--- custom/addons-essentials/todo_ui/__manifest__.py
+++ custom/addons-essentials/todo_ui/__manifest__.py
@@ -4,5 +4,6 @@
     'name': 'User interface improvements to the To-Do app',
     'description': 'User friendly features.',
     'author': 'Daniel Reis',
-    'depends': ['todo_user']
+    'depends': ['todo_user'],
+    'data': ['views/todo_ui_menu.xml']
 }
