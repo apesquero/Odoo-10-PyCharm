86a6dce5768f05c25cba135aa8e80e20030a8f7a
apesquero <apesquero@gmail.com>
2018-03-07 19:50:07 +0100

    Update changes

--- custom/addons-essentials/todo_ui/__manifest__.py
+++ custom/addons-essentials/todo_ui/__manifest__.py
@@ -5,5 +5,7 @@
     'description': 'User friendly features.',
     'author': 'Daniel Reis',
     'depends': ['todo_user'],
-    'data': ['views/todo_ui_menu.xml']
+    'data': ['views/todo_ui_menu.xml',
+             'views/todo_ui.xml'
+             ]
 }
