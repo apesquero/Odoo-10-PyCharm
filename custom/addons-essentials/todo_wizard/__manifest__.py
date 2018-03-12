7f6d447d5e98a37f4719ac836505e5d953ce3310
apesquero <apesquero@gmail.com>
2018-03-13 00:39:05 +0100

    Update changes

---
+++ custom/addons-essentials/todo_wizard/__manifest__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'To-do Tasks Management Assistant',
+    'description': 'Mass edit your To-Do backlog.',
+    'author': 'Daniel Reis',
+    'depends': ['todo_user'],
+    'data': ['views/todo_wizard_view.xml'],
+}
