0dd8cda05b6460f6e09ea8c3db492a2c1dd33e31
apesquero <apesquero@gmail.com>
2018-03-19 22:50:57 +0100

    Update changes

---
+++ custom/addons-essentials/todo_kanban/__manifest__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'To-Do Kanban',
+    'description': 'Kanban board for to-do tasks.',
+    'author': 'Daniel Reis',
+    'depends': ['todo_ui'],
+    'data': ['views/todo_view.xml']
+}
