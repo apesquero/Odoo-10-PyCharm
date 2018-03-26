cafc11320236478bcb8cd8309e640f216d912461
apesquero <apesquero@gmail.com>
2018-03-26 21:57:04 +0200

    Update changes

---
+++ custom/addons-essentials/todo_report/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'To-Do Report',
+    'description': 'Report for To-Do Tasks.',
+    'author': 'Daniels Reis',
+    'depends': ['todo_kanban'],
+    'data': ['reports/todo_report.xml',
+             'reports/todo_report_assets.xml']
+}
