0ec845010cfc3664a4f5827f3444aea76f227825
apesquero <apesquero@gmail.com>
2018-03-29 23:10:05 +0200

    Update changes

--- custom/addons-essentials/todo_website/models/todo_task.py
+++ custom/addons-essentials/todo_website/models/todo_task.py
@@ -10,6 +10,6 @@ class TodoTask(models.Model):
     def website_form_input_filter(self, request, values):
         if 'name' in values:
             values['name'] = values['name'].strip()
-            if len(values.['name']) < 3:
+            if len(values['name']) < 3:
                 raise ValidationError('Text must be at least 3 characters long')
         return values
