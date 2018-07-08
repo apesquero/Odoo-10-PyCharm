6d2dc9c785d80ff3d4c437a37729edf66d60bb8f
apesquero <apesquero@gmail.com>
2018-07-08 18:40:14 +0200

    Update Changes

--- custom/addons/sale_price_dimension/models/inherited_sale_order_line.py
+++ custom/addons/sale_price_dimension/models/inherited_sale_order_line.py
@@ -186,3 +186,31 @@ class SaleOrderLine(models.Model):
                                                 self.tax_id,
                                                 self.company_id)
         return res
+
+    @api.multi
+    def _get_display_price(self, product):
+        # TO DO: move me in master/saas-16 on sale.order
+        if self.order_id.pricelist_id.discount_policy == 'with_discount'\
+                or product._name != 'product.product':
+            return product.with_context(pricelist=self.order_id.pricelist_id.id).price
+
+        final_price, rule_id = self.order_id.pricelist_id.\
+            get_product_price_rule(self.product_id,
+                                   self.product_uom_qty or 1.0,
+                                   self.order_id.partner_id)
+
+        context_partner = dict(self.env.context, partner_id=self.order_id.partner_id.id,
+                               date=self.order_id.date_order)
+
+        base_price, currency_id = self.with_context(context_partner).\
+            _get_real_price_currency(self.product_id, rule_id,
+                                     self.product_uom_qty,
+                                     self.product_uom,
+                                     self.order_id.pricelist_id.id)
+        if currency_id != self.order_id.pricelist_id.currency_id.id:
+            base_price = self.env['res.currency'].browse(currency_id).\
+                with_context(context_partner).compute(base_price,
+                                                      self.order_id.pricelist_id.currency_id)
+
+        # negative discounts (= surcharge) are included in the display price
+        return max(base_price, final_price)
