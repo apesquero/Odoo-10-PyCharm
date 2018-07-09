1e6ba8f2c2a49a34ec4b5f187b4f925cec562c1f
apesquero <apesquero@gmail.com>
2018-07-09 23:49:13 +0200

    Update Changes

--- custom/addons/sale_price_dimension/models/inherited_sale_order_line.py
+++ custom/addons/sale_price_dimension/models/inherited_sale_order_line.py
@@ -68,15 +68,17 @@ class SaleOrderLine(models.Model):
         title = False
         message = False
         warning = {}
-        if product.sale_line_warn != 'no-message':
-            title = _("Warning for %s") % product.name
-            message = product.sale_line_warn_msg
-            warning['title'] = title
-            warning['message'] = message
-            result = {'warning': warning}
-            if product.sale_line_warn == 'block':
-                self.product_id = False
-                return result
+        # Avoid the message if you have not completed the attributes and have not defined the product
+        if product.id is not False:
+            if product.sale_line_warn != 'no-message':
+                title = _("Warning for %s") % product.name
+                message = product.sale_line_warn_msg
+                warning['title'] = title
+                warning['message'] = message
+                result = {'warning': warning}
+                if product.sale_line_warn == 'block':
+                    self.product_id = False
+                    return result
 
         if self.product_tmpl_id.sale_price_type not in ['table_1d', 'table_2d', 'area']:
             self.origin_height = self.origin_width = 0
