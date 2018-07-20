d605d68a392769b79ec9edd8bfb516b0a5bba5c9
apesquero <apesquero@gmail.com>
2018-07-20 20:59:55 +0200

    Update Change

---
+++ custom/addons/account_variant_configurator/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Account Variant Configurator',
+    'description': 'Module that expands the way of calculating the price according to the dimensions, for the products of sale',
+    'version': '10.0.1.0',
+    'author': 'Amaro Pesquero',
+    'data': [
+            'views/account_invoice_view.xml',
+            ],
+    'category': 'Accounting',
+    'depends': ['account',
+                ],
+    'application': True,
+    'installable': True,
+}
---
+++ custom/addons/account_variant_configurator/models/account_invoice.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from odoo import models
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = ['account.invoice.line', 'product.configurator']
+    _name = "account.invoice.line"
